39f81283c8c0b61a781dffcd7e03744ebc7c7408
alberba <albertsalomv@gmail.com>
2025-05-11 12:21:54 +0200

    Actualizar la URL base del scraper de Substack

--- substack_scraper.py
+++ substack_scraper.py
@@ -24,7 +24,7 @@ from urllib.parse import urlparse
 from config import EMAIL, PASSWORD
 
 USE_PREMIUM: bool = False  # Set to True if you want to login to Substack and convert paid for posts
-BASE_SUBSTACK_URL: str = "https://open.substack.com/pub/galicianinvestor/p/71-por-que-importa-el-analisis-sectorial?r=1nxdxj&utm_campaign=post&utm_medium=web"  # Substack you want to convert to markdown
+BASE_SUBSTACK_URL: str = "https://www.thefitzwilliam.com/"  # Substack you want to convert to markdown
 BASE_MD_DIR: str = "substack_md_files"  # Name of the directory we'll save the .md essay files
 BASE_HTML_DIR: str = "substack_html_pages"  # Name of the directory we'll save the .html essay files
 HTML_TEMPLATE: str = "author_template.html"  # HTML template to use for the author page
